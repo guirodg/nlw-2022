82dce8b5490f63607e8846ee206fae68dc8339a9
guirodg <guilherme.rddev@gmail.com>
2022-09-16 13:33:54 -0300

    att repo

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,10 +5,15 @@ module.exports = {
     './index.html'
   ],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+
+    },
     extend: {
       backgroundImage: {
         galaxy: "url('/backgound.png')",
-        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 16.08%, #43E7AD 33.94%, #E1D55D 55.57%)'
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 16.08%, #43E7AD 33.94%, #E1D55D 55.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
       }
     },
   },
